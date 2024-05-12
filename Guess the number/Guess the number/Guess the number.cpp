d6476cca7f24e5d4380f96b61bb3f8c5f1e1a236
Fepsid <vindovspro7@gmail.com>
2024-05-12 11:26:02 +0300

    Guess the number

---
+++ Guess the number/Guess the number/Guess the number.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <time.h>
+
+int main() {
+    bool restart = true;
+    short number;
+    std::cout << "=========================================================================" << std::endl;
+    std::cout << "Guess the number from 1 to 10" << "                                           =" << std::endl;
+    std::cout << "=========================================================================" << std::endl;
+    do {
+        srand(time(NULL));
+        short rezult = 1 + rand() % 10;
+        std::cout << "Number: ";
+        std::cin >> number;
+        if (number == rezult) {
+            std::cout << "=========================================================================" << std::endl;
+            std::cout << "You guessed it!" << "                                                         =" << std::endl;
+            std::cout << "End game" << "                                                                =" << std::endl;
+            std::cout << "=========================================================================" << std::endl;
+            restart = false;
+        }
+        else {
+            std::cout << "Guessed wrong" << std::endl;
+            restart = true;
+        }
+    } while (restart == true);
+
+    system("pause");
+    return 0;
+}
